2c9588945a7b1e508cd7db5e85997b417433981c
samchat2561 <samchat2561@gmail.com>
2026-06-09 10:30:57 +0700

    update

--- server.ts
+++ server.ts
@@ -1,5 +1,10 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 const app: Application = express();
+
+// 1.GET http://localhost:3000/
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello, World!");
+});
 
 const PORT: number = 3000;
 app.listen(PORT, (): void => {
